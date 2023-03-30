c7256238d6e7ead0914291738ae51396202b0931
nomnomnonono <murumuru1204@gmail.com>
2023-03-30 14:36:14 +0900

    add keyword search method

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ axXiv APIのPythonラッパーであるarxivライブラリを用いて取得し
 Hugging Faceにて機械学習における公平性の論文検索アプリを公開しているので是非ご活用ください。[link](https://huggingface.co/spaces/nomnomnonono/fairness-paper-search)
 
 ## TODO
-- キーワード検索機能の追加
 - 他分野の検索を可能にする
 - 検索精度の向上
+  - pkeでキーワード抽出を行い、キーワードベクトルを作成して検索することを試みたが、抽出されるキーワードの精度が低かったため中止した
+  - Sentence BERTのFTをしたいが、ある文章と類似する文章のペアをどのように作成するかを検討中
--- search.py
+++ search.py
@@ -16,7 +16,7 @@ class Search:
             os.path.join(self.config.path_data, "abstract_embed.npy")
         )
         self.df = pd.read_csv(os.path.join(self.config.path_data, "paper.csv"))[
-            ["title", "author", "link"]
+            ["title", "abstract", "link"]
         ]
         self.model = SentenceTransformer(self.config.bert_model)
 
@@ -30,5 +30,16 @@ class Search:
         pred = self.model.encode([abst]).squeeze()
         prob = np.dot(self.abst_embed, pred)
         rank = np.argsort(prob)[::-1]
-        print(rank)
+        return self.df.iloc[rank[0 : int(top)]]
+
+    def search_keyword(self, key1, key2, key3, target, top):
+        keyword_counts = []
+        for i in range(len(self.df)):
+            line = self.df.iloc[i][target.lower()].lower()
+            count = 0
+            for keyword in [key1, key2, key3]:
+                if keyword.lower() in line:
+                    count += 1
+            keyword_counts.append(count)
+        rank = np.argsort(np.array(keyword_counts))[::-1]
         return self.df.iloc[rank[0 : int(top)]]
